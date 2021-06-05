df112d8e7c788f41490d0190e5bb1f20946bf3e0
qiuyuli <qiuyuli@Ctrip.com>
2021-06-05 13:24:44 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react'
+import { Map, Marker, NavigationControl, InfoWindow } from 'react-bmapgl';
+
+function App() {
+  return (
+    <Map center={{ lng: 116.402544, lat: 39.928216 }} zoom="11">
+      <Marker position={{ lng: 116.402544, lat: 39.928216 }} />
+      <NavigationControl />
+      <InfoWindow position={{ lng: 116.402544, lat: 39.928216 }} text="内容" title="标题" />
+    </Map>
+  );
+}
+
+export default App;
