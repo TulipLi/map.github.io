9094b4c77243b30a2ff7ed2e00fa2e7bbed74dbd
qiuyuli <qiuyuli@trip.com>
2022-11-18 16:06:14 +0800

    change to hash route

--- src/App.js
+++ src/App.js
@@ -3,34 +3,21 @@
 import './App.css';
 import React from 'react'
 import {
-	BrowserRouter as Router,
 	Switch,
 	Route,
 } from 'react-router-dom';
 import HuSu from '../src/husu/HuSu'
 
 function App() {
-	return <Router>
-		<div>
-			<Switch>
-				<Route exact path="/">
-					<div>你好～</div>
-				</Route>
-				<Route path="/husu">
-					<HuSu />
-				</Route>
-				<Route path="/map.github.io/husu">
-					<HuSu />
-				</Route>
-				<Route path="/map.github.io">
-					<div>你好～</div>
-				</Route>
-				<Route path="/test">
-					<div>test</div>
-				</Route>
-			</Switch>
-		</div>
-	</Router>
+	return <Switch>
+		<Route exact path="/"></Route>
+		<Route path="/husu">
+			<HuSu />
+		</Route>
+		<Route path="/test">
+			<div>test</div>
+		</Route>
+	</Switch>
 }
 
 export default App;
